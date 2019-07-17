14c1ee792bb2f9ca9d367efefed3b70a259539cd
Jaime Vinas <jvinas@songtrust.com>
2019-07-17 13:22:04 -0400

    fixed indentation

--- experimental.py
+++ experimental.py
@@ -7,61 +7,61 @@ from experimental_config import ENABLED_EXPERIMENTS
 EXPERIMENTS_IGNORED = False
 
 class DisabledExperiment(Exception):
-	pass
+    pass
 
 class MismatchingArguments(TypeError):
-	pass
+    pass
 
 def volatile(experiment, safe=False, refactor=False):
-	if not ENABLED_EXPERIMENTS:
-		raise Exception("ENABLED EXPERIMENTS NOT DEFINED")
-        @wrapt.decorator
-	def wrapper(subject, instance, args, kwargs):
-            if EXPERIMENTS_IGNORED:
-                return subject(*args, **kwargs)
+    if not ENABLED_EXPERIMENTS:
+        raise Exception("ENABLED EXPERIMENTS NOT DEFINED")
+    @wrapt.decorator
+    def wrapper(subject, instance, args, kwargs):
+        if EXPERIMENTS_IGNORED:
+            return subject(*args, **kwargs)
 
-            subject_arguments_count = len(inspect.getargspec(subject).args)
-            experiment_arguments_count = len(inspect.getargspec(experiment).args)
-            if subject_arguments_count != experiment_arguments_count:
-                    raise MismatchingArguments(
-                            "Subjects and experiments must have the same number of arguments. '{}' has {} arguments while '{}' has {}.".format(
-                                    subject.__name__,
-                                    subject_arguments_count,
-                                    experiment.__name__,
-                                    experiment_arguments_count
-                            )
-                    ) # TODO(jaimevp54): Add number of arguments to error message
+        subject_arguments_count = len(inspect.getargspec(subject).args)
+        experiment_arguments_count = len(inspect.getargspec(experiment).args)
+        if subject_arguments_count != experiment_arguments_count:
+            raise MismatchingArguments(
+                    "Subjects and experiments must have the same number of arguments. '{}' has {} arguments while '{}' has {}.".format(
+                            subject.__name__,
+                            subject_arguments_count,
+                            experiment.__name__,
+                            experiment_arguments_count
+                    )
+            ) # TODO(jaimevp54): Add number of arguments to error message
+                
+        if "*" not in ENABLED_EXPERIMENTS and experiment.__name__ not in ENABLED_EXPERIMENTS:
+            return subject(*args, **kwargs)
+        else:
+            #print("Running '"+ subject.__name__ + "' as experimental function '" + experiment.__name__ +"'.")
+            try:
+                experiment_result = experiment(*args, **kwargs)
+                subject_result = subject(*args, **kwargs)
+                if refactor and experiment_result != subject_result:
+                    return subject_result
+                return experiment_result
                     
-            if "*" not in ENABLED_EXPERIMENTS and experiment.__name__ not in ENABLED_EXPERIMENTS:
+            except:
+                if safe:
+                    #print("WARNING: There was an error while executing '"+ experiment.__name__ + "' falling back to '" + func.__name__ +"'.")
                     return subject(*args, **kwargs)
-            else:
-                    #print("Running '"+ subject.__name__ + "' as experimental function '" + experiment.__name__ +"'.")
-                    try:
-                            experiment_result = experiment(*args, **kwargs)
-                            subject_result = subject(*args, **kwargs)
-                            if refactor and experiment_result != subject_result:
-                                    return subject_result
-                            return experiment_result
-                            
-                    except:
-                            if safe:
-                                    #print("WARNING: There was an error while executing '"+ experiment.__name__ + "' falling back to '" + func.__name__ +"'.")
-                                    return subject(*args, **kwargs)
-                            else:
-                                    raise
-	return wrapper
+                else:
+                    raise
+    return wrapper
 
 def experiment(identifier=None):
-        @wrapt.decorator
-	def wrapper(wrapped, instance, args, kwargs):
-            if EXPERIMENTS_IGNORED:
-                return wrapped(*args, **kwargs)
-
-            _identifier = identifier or wrapped.__name__
-            if '*' not in ENABLED_EXPERIMENTS and _identifier not in ENABLED_EXPERIMENTS:
-                    raise DisabledExperiment("'"+wrapped.__name__ + "' is  a experimental feature and it has not been enabled")
+    @wrapt.decorator
+    def wrapper(wrapped, instance, args, kwargs):
+        if EXPERIMENTS_IGNORED:
             return wrapped(*args, **kwargs)
-	return wrapper
+
+        _identifier = identifier or wrapped.__name__
+        if '*' not in ENABLED_EXPERIMENTS and _identifier not in ENABLED_EXPERIMENTS:
+            raise DisabledExperiment("'"+wrapped.__name__ + "' is  a experimental feature and it has not been enabled")
+        return wrapped(*args, **kwargs)
+    return wrapper
 
 
 class experimental_block():
--- tests.py
+++ tests.py
@@ -3,149 +3,148 @@ import experimental
 from experimental import volatile, experiment, DisabledExperiment, MismatchingArguments, experimental_block, experiments_ignored
 from mock import patch
 class TestVolatileDecorator(unittest.TestCase):
-	def test_calls_enabled_experiment(self):
-		@experiment()
-		def e_func():
-			return id(e_func)
-			
-		@volatile(experiment=e_func)
-		def v_func():
-			return id(v_func)
-		
-		with patch('experimental.ENABLED_EXPERIMENTS', []):
-			self.assertEqual(id(v_func), v_func())
+    def test_calls_enabled_experiment(self):
+        @experiment()
+        def e_func():
+                return id(e_func)
+                
+        @volatile(experiment=e_func)
+        def v_func():
+                return id(v_func)
+        
+        with patch('experimental.ENABLED_EXPERIMENTS', []):
+                self.assertEqual(id(v_func), v_func())
 
-		with patch('experimental.ENABLED_EXPERIMENTS', ['other_func']):
-			self.assertEqual(id(v_func), v_func())
+        with patch('experimental.ENABLED_EXPERIMENTS', ['other_func']):
+                self.assertEqual(id(v_func), v_func())
 
-		with patch('experimental.ENABLED_EXPERIMENTS', ['*']):
-			self.assertEqual(id(e_func), v_func())
+        with patch('experimental.ENABLED_EXPERIMENTS', ['*']):
+                self.assertEqual(id(e_func), v_func())
 
-		with patch('experimental.ENABLED_EXPERIMENTS', ['e_func']):
-			self.assertEqual(id(e_func), v_func())
+        with patch('experimental.ENABLED_EXPERIMENTS', ['e_func']):
+                self.assertEqual(id(e_func), v_func())
 
-	def test_different_number_of_arguments_raises_exception(self):
-		@experiment()
-		def e_func():
-			return id(e_func)
-			
-		@volatile(experiment=e_func)
-		def v_func(a):
-			return id(v_func)
-		
-		with patch('experimental.ENABLED_EXPERIMENTS', ['*']):
-			with self.assertRaises(MismatchingArguments):
-				v_func("FAIL")
+    def test_different_number_of_arguments_raises_exception(self):
+        @experiment()
+        def e_func():
+            return id(e_func)
+                
+        @volatile(experiment=e_func)
+        def v_func(a):
+            return id(v_func)
+        
+        with patch('experimental.ENABLED_EXPERIMENTS', ['*']):
+            with self.assertRaises(MismatchingArguments):
+                v_func("FAIL")
 
-	def test_safe_flag(self):
-		@experiment()
-		def e_func():
-			raise Exception
-			return id(e_func)
-			
-		@volatile(experiment=e_func, safe=True)
-		def v_func():
-			return id(v_func)
-		
-		with patch('experimental.ENABLED_EXPERIMENTS', ['*']):
-			self.assertEqual(id(v_func), v_func())
+    def test_safe_flag(self):
+        @experiment()
+        def e_func():
+            raise Exception
+            return id(e_func)
+                
+        @volatile(experiment=e_func, safe=True)
+        def v_func():
+            return id(v_func)
+        
+        with patch('experimental.ENABLED_EXPERIMENTS', ['*']):
+            self.assertEqual(id(v_func), v_func()) 
+    def test_refactor_flag(self):
+            
+        @experiment()
+        def bad_refactor(original_list):
+            if original_list == [42] or original_list == None:
+                return list(original_list)
+            return original_list
+                
+        @volatile(experiment=bad_refactor, refactor=True)
+        def get_same_list(original_list):
+            return original_list
+        
+        with patch('experimental.ENABLED_EXPERIMENTS', ['*']):
+            a_list = []
+            self.assertEqual(id(get_same_list(a_list)), id(a_list))
+            
+            a_list = [42]
+            self.assertNotEqual(id(get_same_list(a_list)), id(a_list))
+            
+            with self.assertRaises(Exception):
+                get_same_list(None)
+            
+        with patch('experimental.ENABLED_EXPERIMENTS', []):
+            a_list = []
+            self.assertEqual(id(get_same_list(a_list)), id(a_list))
+            
+            a_list = [42]
+            self.assertEqual(id(get_same_list(a_list)), id(a_list))
 
-	def test_refactor_flag(self):
-		
-		@experiment()
-		def bad_refactor(original_list):
-                    if original_list == [42] or original_list == None:
-                        return list(original_list)
-                    return original_list
-			
-		@volatile(experiment=bad_refactor, refactor=True)
-                def get_same_list(original_list):
-			return original_list
-		
-		with patch('experimental.ENABLED_EXPERIMENTS', ['*']):
-                        a_list = []
-			self.assertEqual(id(get_same_list(a_list)), id(a_list))
-			
-                        a_list = [42]
-			self.assertNotEqual(id(get_same_list(a_list)), id(a_list))
-			
-			with self.assertRaises(Exception):
-				get_same_list(None)
-			
-		with patch('experimental.ENABLED_EXPERIMENTS', []):
-                        a_list = []
-			self.assertEqual(id(get_same_list(a_list)), id(a_list))
-			
-                        a_list = [42]
-			self.assertEqual(id(get_same_list(a_list)), id(a_list))
-
-			
+                    
 
 		
 
 	
 class TestExperimentDecorator(unittest.TestCase):
-	def test_experimental_function_raises_exception(self):
-		@experiment()
-		def func():
-			pass
+    def test_experimental_function_raises_exception(self):
+        @experiment()
+        def func():
+            pass
 
-		with patch('experimental.ENABLED_EXPERIMENTS', []):
-			with self.assertRaises(DisabledExperiment):
-				func()
+        with patch('experimental.ENABLED_EXPERIMENTS', []):
+            with self.assertRaises(DisabledExperiment):
+                func()
 
-		with patch('experimental.ENABLED_EXPERIMENTS', ['otherFunction']):
-			with self.assertRaises(DisabledExperiment):
-				func()
+        with patch('experimental.ENABLED_EXPERIMENTS', ['otherFunction']):
+            with self.assertRaises(DisabledExperiment):
+                func()
 
-		with patch('experimental.ENABLED_EXPERIMENTS', ['*']):
-			try:
-				func()
-			except DisabledExperiment:
-				self.fail("func() raised DisabledExperiment unexpectedly!")
+        with patch('experimental.ENABLED_EXPERIMENTS', ['*']):
+            try:
+                func()
+            except DisabledExperiment:
+                self.fail("func() raised DisabledExperiment unexpectedly!")
 
-		with patch('experimental.ENABLED_EXPERIMENTS', ['func']):
-			try:
-				func()
-			except DisabledExperiment:
-				self.fail("func() raised DisabledExperiment unexpectedly!")
+        with patch('experimental.ENABLED_EXPERIMENTS', ['func']):
+            try:
+                func()
+            except DisabledExperiment:
+                self.fail("func() raised DisabledExperiment unexpectedly!")
 
-	def test_identifier(self):
-		@experiment('feature_A')
-		def func():
-			pass
+    def test_identifier(self):
+        @experiment('feature_A')
+        def func():
+            pass
 
-		with patch('experimental.ENABLED_EXPERIMENTS', ['feature_A']):
-			try:
-				func()
-			except DisabledExperiment:
-				self.fail("func() raised DisabledExperiment unexpectedly!")
+        with patch('experimental.ENABLED_EXPERIMENTS', ['feature_A']):
+            try:
+                func()
+            except DisabledExperiment:
+                self.fail("func() raised DisabledExperiment unexpectedly!")
 
-	def test_experiments_ignored_blocks(self):
-		@experiment()
-		def e_func():
-			return id(e_func)
-			
-		@volatile(experiment=e_func, safe=True)
-		def v_func():
-			return id(v_func)
-		
-		with patch('experimental.ENABLED_EXPERIMENTS', ['*']):
-                    self.assertEqual(id(e_func), v_func())
+    def test_experiments_ignored_blocks(self):
+        @experiment()
+        def e_func():
+            return id(e_func)
+                
+        @volatile(experiment=e_func, safe=True)
+        def v_func():
+            return id(v_func)
+        
+        with patch('experimental.ENABLED_EXPERIMENTS', ['*']):
+            self.assertEqual(id(e_func), v_func())
 
-                    with experiments_ignored():
-			self.assertEqual(id(v_func), v_func())
+            with experiments_ignored():
+                self.assertEqual(id(v_func), v_func())
 
 
 class TestExperimentalBlock(unittest.TestCase):
-	def test_disabled_block_raises_exception(self):
-            with patch('experimental.ENABLED_EXPERIMENTS', []):
-                with self.assertRaises(DisabledExperiment):
-                    with experimental_block('test'):
-                        pass 
-
-            with patch('experimental.ENABLED_EXPERIMENTS', ['*']):
+    def test_disabled_block_raises_exception(self):
+        with patch('experimental.ENABLED_EXPERIMENTS', []):
+            with self.assertRaises(DisabledExperiment):
                 with experimental_block('test'):
                     pass 
+
+        with patch('experimental.ENABLED_EXPERIMENTS', ['*']):
+            with experimental_block('test'):
+                pass 
 
 unittest.main()
